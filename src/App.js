bc2a81d69ff1a600e80d7c5e4fd883ec7b1423e8
Corey Russ <corey.j.russ@gmail.com>
2018-03-11 16:33:57 -0700

    App is functional, needs to be styled correctly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 import { RoomList } from './components/RoomList';
+import { MessageList } from './components/MessageList';
 
 // Initialize Firebase
   var config = {
@@ -15,11 +16,27 @@ import { RoomList } from './components/RoomList';
   firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {activeRoom: '' };
+      this.activeRoom = this.activeRoom.bind(this);
+  }
+
+  activeRoom(room) {
+    this.setState({ activeRoom: room })
+  }
+
+
   render() {
+    const showMessages = this.state.activeRoom;
     return (
-      <div className="App">
-        <h1>Bloc Chat</h1>
-        <RoomList firebase={firebase} />
+      <div>
+          <h1>{this.state.activeRoom.name || "Select A Room"}</h1>
+          <RoomList firebase={firebase} activeRoom={this.activeRoom} />
+          { showMessages ?
+          (<MessageList firebase={firebase} activeRoom={this.state.activeRoom.key}/>)
+          : (null)
+          }
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,13 +5,15 @@ export class RoomList extends Component {
     super(props);
       this.state = { name: '', rooms: [] };
       this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.handleChange = this.handleChange.bind(this);
+      this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) })
+      this.setState({ rooms: this.state.rooms.concat(room) })
     });
   }
 
@@ -22,20 +24,28 @@ export class RoomList extends Component {
   }
 
   handleChange(e) {
-    this.setState({ name: e.target.value })
+    this.setState({ name: e.target.value });
+  }
+
+  selectRoom(room) {
+    this.props.activeRoom(room);
   }
 
   render() {
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type='text' value={this.state.name} placeholder='Enter New Room Name' onChange={this.handleChange} />
+        <input type='submit' value='Create' />
+      </form>
+    );
+
     const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
+      <li key={room.key} onClick= {(e) => this.selectRoom(room, e) } >{room.name}</li>
   );
     return (
-      <div id="roomListData">
+      <div>
+        <div>{roomForm}</div>
         <ul>{roomList}</ul>
-        <form onSubmit={ (e) => this.createRoom(e) }>
-          <input type='text' value={ this.state.name } placeholder='Enter New Room Name Here' onChange={ (e) => this.handleChange(e) }/>
-          <input type='submit' />
-        </form>
       </div>
     );
   }
